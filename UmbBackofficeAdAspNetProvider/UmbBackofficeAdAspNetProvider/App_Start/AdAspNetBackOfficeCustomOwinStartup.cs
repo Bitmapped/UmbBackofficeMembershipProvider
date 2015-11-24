2eafd4fd6de75592be90edd486f57a383364c0e6
Brian Powell <bpowell@bitmapped.net>
2015-11-24 21:11:36 +0000

    Initial checkin.

---
+++ UmbBackofficeAdAspNetProvider/UmbBackofficeAdAspNetProvider/App_Start/AdAspNetBackOfficeCustomOwinStartup.cs
@@ -0,0 +1,95 @@
+using Microsoft.Owin;
+using Owin;
+using Umbraco.Core;
+using Umbraco.Core.Security;
+using Umbraco.Web.Security.Identity;
+using UmbBackofficeAdAspNetProvider;
+using Umbraco.Core.Models.Identity;
+
+//To use this startup class, change the appSetting value in the web.config called 
+// "owin:appStartup" to be "AdAspNetBackOfficeCustomOwinStartup"
+
+[assembly: OwinStartup("AdAspNetBackOfficeCustomOwinStartup", typeof(AdAspNetBackOfficeCustomOwinStartup))]
+
+namespace UmbBackofficeAdAspNetProvider
+{
+    /// <summary>
+    /// A custom way to configure OWIN for Umbraco
+    /// </summary>
+    /// <remarks>
+    /// The startup type is specified in appSettings under owin:appStartup - change it to "UmbracoCustomOwinStartup" to use this class
+    /// 
+    /// This startup class would allow you to customize the Identity IUserStore and/or IUserManager for the Umbraco Backoffice
+    /// </remarks>
+    public class AdAspNetBackOfficeCustomOwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Configure the Identity user manager for use with Umbraco Back office
+
+            var applicationContext = ApplicationContext.Current;
+            app.ConfigureUserManagerForUmbracoBackOffice<BackOfficeUserManager, BackOfficeIdentityUser>(
+                applicationContext,
+                (options, context) =>
+                {
+                    var membershipProvider = MembershipProviderExtensions.GetUsersMembershipProvider().AsUmbracoMembershipProvider();
+                    var userManager = BackOfficeUserManager.Create(options,
+                        applicationContext.Services.UserService,
+                        applicationContext.Services.ExternalLoginService,
+                        membershipProvider);
+
+                    // Call custom passowrd checker.
+                    userManager.BackOfficeUserPasswordChecker = new AdAspNetBackOfficePasswordChecker();
+
+                    return userManager;
+                });
+
+            //Ensure owin is configured for Umbraco back office authentication
+            app
+                .UseUmbracoBackOfficeCookieAuthentication(ApplicationContext.Current)
+                .UseUmbracoBackOfficeExternalCookieAuthentication(ApplicationContext.Current);
+
+            /* 
+             * Configure external logins for the back office:
+             * 
+             * Depending on the authentication sources you would like to enable, you will need to install 
+             * certain Nuget packages. 
+             * 
+             * For Google auth:					Install-Package UmbracoCms.IdentityExtensions.Google
+             * For Facebook auth:					Install-Package UmbracoCms.IdentityExtensions.Facebook
+             * For Microsoft auth:					Install-Package UmbracoCms.IdentityExtensions.Microsoft
+             * For Azure ActiveDirectory auth:		Install-Package UmbracoCms.IdentityExtensions.AzureActiveDirectory
+             * 
+             * There are many more providers such as Twitter, Yahoo, ActiveDirectory, etc... most information can
+             * be found here: http://www.asp.net/web-api/overview/security/external-authentication-services
+             * 
+             * For sample code on using external providers with the Umbraco back office, install one of the 
+             * packages listed above to review it's code samples 
+             *  
+             */
+
+            /*
+             * To configure a simple auth token server for the back office:
+             *             
+             * By default the CORS policy is to allow all requests
+             * 
+             *      app.UseUmbracoBackOfficeTokenAuth(new BackOfficeAuthServerProviderOptions());
+             *      
+             * If you want to have a custom CORS policy for the token server you can provide
+             * a custom CORS policy, example: 
+             * 
+             *      app.UseUmbracoBackOfficeTokenAuth(
+             *          new BackOfficeAuthServerProviderOptions()
+             *              {
+             *             		//Modify the CorsPolicy as required
+             *                  CorsPolicy = new CorsPolicy()
+             *                  {
+             *                      AllowAnyHeader = true,
+             *                      AllowAnyMethod = true,
+             *                      Origins = { "http://mywebsite.com" }                
+             *                  }
+             *              });
+             */
+        }
+    }
+}
