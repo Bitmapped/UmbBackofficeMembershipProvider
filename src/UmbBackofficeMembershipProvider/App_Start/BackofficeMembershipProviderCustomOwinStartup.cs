d4d2282ea97fa9aa5c3861cc3809752e81807313
bitmapped <bpowell@bitmapped.net>
2016-02-22 17:16:51 -0500

    Remove unused App_Start files to eliminate confusion.

--- src/UmbBackofficeMembershipProvider/App_Start/BackofficeMembershipProviderCustomOwinStartup.cs
+++ src/UmbBackofficeMembershipProvider/App_Start/BackofficeMembershipProviderCustomOwinStartup.cs
@@ -48,48 +48,6 @@ namespace UmbBackofficeMembershipProvider
             app
                 .UseUmbracoBackOfficeCookieAuthentication(ApplicationContext.Current)
                 .UseUmbracoBackOfficeExternalCookieAuthentication(ApplicationContext.Current);
-
-            /* 
-             * Configure external logins for the back office:
-             * 
-             * Depending on the authentication sources you would like to enable, you will need to install 
-             * certain Nuget packages. 
-             * 
-             * For Google auth:					Install-Package UmbracoCms.IdentityExtensions.Google
-             * For Facebook auth:					Install-Package UmbracoCms.IdentityExtensions.Facebook
-             * For Microsoft auth:					Install-Package UmbracoCms.IdentityExtensions.Microsoft
-             * For Azure ActiveDirectory auth:		Install-Package UmbracoCms.IdentityExtensions.AzureActiveDirectory
-             * 
-             * There are many more providers such as Twitter, Yahoo, ActiveDirectory, etc... most information can
-             * be found here: http://www.asp.net/web-api/overview/security/external-authentication-services
-             * 
-             * For sample code on using external providers with the Umbraco back office, install one of the 
-             * packages listed above to review it's code samples 
-             *  
-             */
-
-            /*
-             * To configure a simple auth token server for the back office:
-             *             
-             * By default the CORS policy is to allow all requests
-             * 
-             *      app.UseUmbracoBackOfficeTokenAuth(new BackOfficeAuthServerProviderOptions());
-             *      
-             * If you want to have a custom CORS policy for the token server you can provide
-             * a custom CORS policy, example: 
-             * 
-             *      app.UseUmbracoBackOfficeTokenAuth(
-             *          new BackOfficeAuthServerProviderOptions()
-             *              {
-             *             		//Modify the CorsPolicy as required
-             *                  CorsPolicy = new CorsPolicy()
-             *                  {
-             *                      AllowAnyHeader = true,
-             *                      AllowAnyMethod = true,
-             *                      Origins = { "http://mywebsite.com" }                
-             *                  }
-             *              });
-             */
         }
     }
 }
