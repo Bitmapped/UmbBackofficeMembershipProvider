4dc41305f0e12d2c21897d966d10d93d507af406
Bitmapped <bpowell@bitmapped.net>
2017-09-20 21:57:41 -0400

    Upgrade to 7.7.0, flesh out autolinking

--- src/UmbBackofficeMembershipProvider/BackofficeMembershipProviderPasswordChecker.cs
+++ src/UmbBackofficeMembershipProvider/BackofficeMembershipProviderPasswordChecker.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Umbraco.Core.Models.Identity;
 using Umbraco.Core.Security;
 using System.Web.Security;
+using System.Web;
 
 namespace UmbBackofficeMembershipProvider
 {
@@ -23,8 +24,36 @@ namespace UmbBackofficeMembershipProvider
             }
             var adProvider = Membership.Providers["BackofficeMembershipProvider"];
 
+            // Check the password against Active Directory.
+            var validPassword = adProvider.ValidateUser(user.UserName, password);
+
+            // Automatically create a user account if needed.
+            if (validPassword && !user.HasIdentity)
+            {
+                // Get user from Active Directory and populate into account if possible.
+                var adUser = adProvider.GetUser(user.UserName, false);
+                if (adUser != null)
+                {
+                    user.Name = adUser.UserName;
+                    user.Email = adUser.Email ?? String.Format("{0}@{1}", user.UserName, HttpContext.Current.Request.Url.Host); // TODO: Add suffix if doesn't contain @
+
+                    //TODO: Configure group, culture
+
+                    // Get user manager
+                    var userManager = HttpContext.Current.GetOwinContext().GetBackOfficeUserManager();
+                    if (userManager != null)
+                    {
+                        var createUserTask = userManager.CreateAsync(user);
+                        var createUserResult = createUserTask.Result;
+                        throw new Exception("Gonna stop here");
+                    }
+                    throw new Exception("Gonna stop here");
+                }
+                
+            }
+
             // Check the user's password.
-            var validUser = adProvider.ValidateUser(user.UserName, password) ? Task.FromResult(BackOfficeUserPasswordCheckerResult.ValidCredentials) : Task.FromResult(BackOfficeUserPasswordCheckerResult.InvalidCredentials);
+            var validUser =  validPassword ? Task.FromResult(BackOfficeUserPasswordCheckerResult.ValidCredentials) : Task.FromResult(BackOfficeUserPasswordCheckerResult.InvalidCredentials);
 
             return validUser;
         }
